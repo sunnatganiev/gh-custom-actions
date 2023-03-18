e9b513a4ad8639662d1175d83c0bcf1047e5cf53
macbook <macbook@macbooks-MacBook-Pro.local>
2023-03-18 19:50:30 +0500

    secret key

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -10,7 +10,6 @@ function run() {
 
   // 2) Upload files
   const s3Uri = `s3://${bucket}`;
-  AWS_ACCESS_KEY_ID=AKIAZHNZDQ2TWXFCICNO
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegioin}`);
 
   core.notice('Hello from my custom JS Action!');
