fdaffbaba2e0af84d4901c1ee19e07b71962b873
macbook <macbook@macbooks-MacBook-Pro.local>
2023-03-18 19:48:10 +0500

    fix bug of bucket-region

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -5,7 +5,7 @@ const exec = require('@actions/exec');
 function run() {
   // 1) Get Some Input Values
   const bucket = core.getInput('bucket', { required: true });
-  const bucketRegioin = core.getInput('bucket.region', { required: true });
+  const bucketRegioin = core.getInput('bucket-region', { required: true });
   const distFolder = core.getInput('dist-folder', { required: true });
 
   // 2) Upload files
